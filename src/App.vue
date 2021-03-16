35ad1b01e136e1b3d246632ce9d79741b83377dd
Ann Peng <boaimpact@gmail.com>
2021-03-16 15:12:17 +0800

    project init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style lang="scss">
+@import "assets/scss/main.scss";
+
+#app {
+  color: $mainColor;
+}
+</style>

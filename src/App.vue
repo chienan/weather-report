8d18488c574adbff311ba354215094006c3f17f9
Ann Peng <boaimpact@gmail.com>
2021-03-16 17:24:49 +0800

    add basic scss setting

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,78 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div id="weather-report">
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+// @import "bootstrap";
+// @import "bootstrap/dist/css/bootstrap.min.css";
+
+/* CSS reset */
+body {
+  margin: 0;
+  padding: 0;
+}
+* {
+  box-sizing: border-box;
+  font-family: "Raleway", sans-serif;
+}
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  margin: 0px;
+  padding: 0px;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+ul,
+ol,
+li {
+  list-style: none;
+  margin: 0px;
+  padding: 0px;
 }
+a,
+a:hover,
+a:focus {
+  text-decoration: none;
+}
+img {
+  display: block;
+  object-fit: cover;
+}
+.btn,
+button,
+button:focus,
+input:focus,
+textarea:focus,
+select,
+select:focus,
+a:focus {
+  outline: 0px !important;
+}
+button {
+  background-color: transparent;
+  border: 0;
+  cursor: pointer;
+}
+button[disabled] {
+  pointer-events: none;
+}
+
+// #nav {
+//   padding: 30px;
+
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
+
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
 </style>
---
+++ src/views/Index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container">
+    <div class="nav">Weather Report</div>
+
+    <div class="d-flex card-group">
+      <div class="main-card">
+        <div class="date">Today</div>
+        <div class="temperature">16°C</div>
+        <div class="icon">^____^</div>
+      </div>
+
+      <div class="d-flex sub-card-group">
+        <div class="sub-card">
+          <div class="sub-date">Thursday</div>
+          <div class="sub-temperature">25°C</div>
+          <div class="sub-icon">@......@</div>
+        </div>
+        <div class="sub-card">
+          <div class="sub-date">Thursday</div>
+          <div class="sub-temperature">25°C</div>
+          <div class="sub-icon">@......@</div>
+        </div>
+        <div class="sub-card">
+          <div class="sub-date">Thursday</div>
+          <div class="sub-temperature">25°C</div>
+          <div class="sub-icon">@......@</div>
+        </div>
+        <div class="sub-card">
+          <div class="sub-date">Thursday</div>
+          <div class="sub-temperature">25°C</div>
+          <div class="sub-icon">@......@</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import "../assets/scss/main.scss";
+
+* {
+  border: 1px solid gray;
+}
+
+.card-group {
+  width: 85%;
+}
+.main-card {
+  width: 35%;
+  height: 40%;
+}
+.sub-card-group {
+  border: 1px solid salmon;
+  width: 25%;
+  height: 20%;
+}
+
+/* .main-card,
+.sub-card {
+} */
+</style>

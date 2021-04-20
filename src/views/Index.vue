ae96f6489c4be853445fcf38e5752c9aa538fa91
Ann Peng <boaimpact@gmail.com>
2021-04-20 22:33:49 +0800

    debug: mode change background

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,56 +1,58 @@
 <template>
   <div id="wrapper" :class="{ 'theme-light': lightMode }">
-    <div class="container d-flex justify-content-center">
-      <div class="content-wrapper">
-        <div class="nav px-4 py-4">
-          <div class="nav-title">
-            Weather Report
-            <i
-              class="fas fa-sun switch-mode"
-              v-if="!lightMode"
-              @click="lightMode = !lightMode"
-            ></i>
-            <i class="fas fa-moon switch-mode" v-else @click="lightMode = !lightMode"></i>
-          </div>
-
-          <div class="nav-search">
-            <form>
-              <div class="search-section py-4">
-                <label for="search" class="search"></label>
-                <input
-                  type="place"
-                  class="form-control search-input"
-                  id="search"
-                  placeholder="search for a place"
-                  v-model="location"
-                />
-                <button
-                  type="submit"
-                  class="btn btn-primary mx-4"
-                  @click.stop.prevent="locationSubmit"
-                >search</button>
-              </div>
-            </form>
-          </div>
-
-          <!-- <button v-if="!lightMode" @click="lightMode = !lightMode">Light mode</button>
-          <button v-else @click="lightMode = !lightMode">Dark mode</button>-->
-        </div>
-
-        <div class="card-group">
-          <div class="main-card">
-            <div class="location-section">{{weather.name}}</div>
-
-            <div class="date">{{currentDate}}</div>
-            <div class="temperature">{{Math.round(weather.main.temp)}}°C</div>
-            <div class="icon-wrapper">
-              <i :class="getIcon()"></i>
+    <div class="main-content">
+      <div class="container d-flex justify-content-center">
+        <div class="content-wrapper">
+          <div class="nav px-4 py-4">
+            <div class="nav-title">
+              Weather Report
+              <i
+                class="fas fa-sun switch-mode"
+                v-if="!lightMode"
+                @click="lightMode = !lightMode"
+              ></i>
+              <i class="fas fa-moon switch-mode" v-else @click="lightMode = !lightMode"></i>
             </div>
-            <div class="weather" id="weather">{{weather.weather[0].main}}</div>
+
+            <div class="nav-search">
+              <form>
+                <div class="search-section py-4">
+                  <label for="search" class="search"></label>
+                  <input
+                    type="place"
+                    class="form-control search-input"
+                    id="search"
+                    placeholder="search for a place"
+                    v-model="location"
+                  />
+                  <button
+                    type="submit"
+                    class="btn btn-primary mx-4"
+                    @click.stop.prevent="locationSubmit"
+                  >search</button>
+                </div>
+              </form>
+            </div>
+
+            <!-- <button v-if="!lightMode" @click="lightMode = !lightMode">Light mode</button>
+            <button v-else @click="lightMode = !lightMode">Dark mode</button>-->
+          </div>
+
+          <div class="card-group">
+            <div class="main-card">
+              <div class="location-section">{{weather.name}}</div>
+
+              <div class="date">{{currentDate}}</div>
+              <div class="temperature">{{Math.round(weather.main.temp)}}°C</div>
+              <div class="icon-wrapper">
+                <i :class="getIcon()"></i>
+              </div>
+              <div class="weather" id="weather">{{weather.weather[0].main}}</div>
+            </div>
           </div>
         </div>
+        <!-- <link rel="stylesheet" href="<%= BASE_URL %>css/lighttheme.css" /> -->
       </div>
-      <!-- <link rel="stylesheet" href="<%= BASE_URL %>css/lighttheme.css" /> -->
     </div>
   </div>
 </template>
@@ -116,13 +118,19 @@ export default {
 @import "../assets/scss/main.scss"
 @import url('https://use.fontawesome.com/releases/v5.8.1/css/all.css')
 
-#wrapper
+// #wrapper
+//   background-color: $darkblue
+
+// #wrapper ~.theme-light
+//   background-color: $lightblue
+
+.main-content
   background-color: $darkblue
 
 .container
-  width: 100vw
+  width: 100%
   height: 100vw
-  // background-color: $darkblue
+  background-color: $darkblue
 
   .content-wrapper
     width: 85%
@@ -172,13 +180,16 @@ export default {
       font-size: 20px
 
 .theme-light
-  background-color: $backgroundlight
+  .main-content
+    background-color: $backgroundlight
+
+    background-color: $backgroundlight
 
   .container
     background-color: $backgroundlight
-    color: $darkblue
+    color: $middleblue
 
     .main-card
-      border: 2px solid $darkblue
-      color: $darkblue
+      border: 2px solid $middleblue
+      color: $middleblue
 </style>
